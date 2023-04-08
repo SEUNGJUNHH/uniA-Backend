cc29b1b723d1a5b63437778627319f3962323153
seungjun <slah0709@ajou.ac.kr>
2023-04-08 20:14:17 +0900

    transactional add and member 과제목록 추가 서비스

--- src/main/java/com/example/unia/assignment/service/AssignmentService.java
+++ src/main/java/com/example/unia/assignment/service/AssignmentService.java
@@ -5,23 +5,31 @@ import com.example.unia.assignment.entity.AssignmentEntity;
 import com.example.unia.assignment.repository.AssignmentRepository;
 import com.example.unia.member.dto.MemberDTO;
 import com.example.unia.member.entity.MemberEntity;
+import com.example.unia.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AssignmentService {
 
     private final AssignmentRepository assignmentRepository;
+    private final MemberRepository memberRepository;
 
 
-    public void createAssignment(AssignmentDTO assignmentDTO) {
+    public void createAssignment(AssignmentDTO assignmentDTO,String memberEmail) {
         AssignmentEntity assignmentEntity = AssignmentEntity.toAssignmentEntity(assignmentDTO);
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberEmail);
+        MemberEntity member = byMemberEmail.get();
         assignmentRepository.save(assignmentEntity);
+        member.getAssignments().add(assignmentEntity);
+        System.out.println(member.getAssignments());
     }
 
     public AssignmentDTO getAssignment(Long assignmentId) {
