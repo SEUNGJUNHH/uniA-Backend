a75baa72603b2bf8e5f882a1d6acb535c452687d
JisinKeo <tntjd9514@naver.com>
2023-05-13 14:59:30 +0900

    .

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,12 +11,12 @@ CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 종료할 애플리케이션이 없습니다." >> ./log.txt
+  echo "> 종료할 애플리케이션이 없습니다." >> $REPOSITORY/log.txt
 else
-  echo "> kill -15 $CURRENT_PID" >> ./log.txt
+  echo "> kill -15 $CURRENT_PID" >> $REPOSITORY/log.txt
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> Deploy - $JAR_PATH " >> ./log.txt
+echo "> Deploy - $JAR_PATH " >> $REPOSITORY/log.txt
 nohup java -jar -Dspring.profiles.active=dev $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &
