307ca9edc88c6ebf0d9c7e62bfdb5d027eee66a6
JisinKeo <tntjd9514@naver.com>
2023-05-30 13:14:37 +0900

    Feat getLikedPlaceNamesByMemberId

--- src/main/java/com/example/unia/memberLikes/repository/MemberLikesRepository.java
+++ src/main/java/com/example/unia/memberLikes/repository/MemberLikesRepository.java
@@ -3,6 +3,12 @@ package com.example.unia.memberLikes.repository;
 import com.example.unia.memberLikes.entity.MemberLikesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MemberLikesRepository extends JpaRepository<MemberLikesEntity, Long> {
+    List<MemberLikesEntity> findAllByMemberId(Long memberId);
+
     MemberLikesEntity findByMemberIdAndPlaceId(Long memberId, Long id);
+
 }
